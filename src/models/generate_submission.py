c78f8b9a3658b5f44fcb5bb08074e86582439519
raishish <rashish3@student.nitw.ac.in>
2017-07-16 13:42:51 +0000

    minor changes

--- src/models/generate_submission.py
+++ src/models/generate_submission.py
@@ -2,20 +2,28 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 from keras.models import load_model
-import os, sys
+import os, sys, math, re
 import numpy as np
 import pandas as pd
 from skimage import io
 
 # to make sure tf uses cpu, cause gpu busy
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+# os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+# supress TF warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 model_savePath = os.path.abspath('../../models/VGG19_model_4.hdf5')
-model_weights = os.path.abspath('../../models/VGG19_12weights.08-1.88.hdf5')
-batch_size = 64
+model_weights = os.path.abspath('../../models/VGG19_12/weights.24-2.08.hdf5')
+batch_size = 32
 root_dir = os.path.abspath('../../models/VGG19_12/')
 test_dir = os.path.abspath('../../data/raw/test-tif-v2')
+submission_df_file = os.path.join(root_dir, 'submission_SGD_25.csv')
+
+# get rid of previous results
+if os.path.exists(submission_df_file):
+	os.remove(submission_df_file)
 
 if os.path.exists(model_savePath):
 	model = load_model(model_savePath)
@@ -32,50 +40,53 @@ else:
 	sys.exit(-1)
 
 nb_test_samples = len([name for name in os.listdir(test_dir) if os.path.isfile(os.path.join(test_dir, name))])
+# one more batch to cover the last files
+nb_batches = int(math.ceil(nb_test_samples/(batch_size*1.0)))
 
-nb_batches = nb_test_samples // batch_size
-
-idx = 0
-batch = 0
+print("Found {} testing samples".format(nb_test_samples))
+print("Batches:", nb_batches)
 
 submission_df = pd.DataFrame(columns=['image_name', 'tags'])
 labels = [['clear', 'cloudy', 'haze', 'partly_cloudy'],
-          ['primary', 'water', 'cultivation', 'habitation', 'bare_ground', 'agriculture', 'road',],
-          ['slash_burn', 'blooming', 'conventional_mine', 'artisinal_mine', 'blow_down', 'selective_logging']]
+		  ['primary', 'water', 'cultivation', 'habitation', 'bare_ground', 'agriculture', 'road',],
+		  ['slash_burn', 'blooming', 'conventional_mine', 'artisinal_mine', 'blow_down', 'selective_logging']]
 
-for file_idx, file in enumerate(os.listdir(test_dir)):
+batch = 0
+
+for batch in xrange(nb_batches):
 	batch_inputs = []
 	batch_file_names = []
-	idx = min(batch_size, nb_test_samples - (batch + 1)*batch_size)
+	upper_bound = min((batch + 1)*batch_size, nb_test_samples)
+	for file in os.listdir(test_dir)[batch*batch_size: upper_bound]:
 
-	while idx > 0:
 		image_path = os.path.join(test_dir, file)
-		img = io.imread().astype(np.float32)
+		img = io.imread(image_path).astype(np.float32)
 		for i in range(img.shape[-1]):
 			img[:,:,i] = (img[:,:,i] - img[:,:,i].min())/(img[:,:,i].max() - img[:,:,i].min())
 
 		batch_file_names.append(image_path.split('/')[-1].split('.')[0])
 		batch_inputs.append(img)
 
-	idx -= 1
-
 	batch_inputs = np.array(batch_inputs)
 	batch_preds = model.predict_on_batch(batch_inputs)
 
-	for file_name, preds in zip(batch_file_names, *batch_preds):
+	for file_name, preds in zip(batch_file_names, zip(*batch_preds)):
 		sorted_preds = [np.argsort(pred)[::-1] for pred in preds[:3]]
 		counts = [np.argmax(np_count) for np_count in preds[3:]]
 		tags = ''
 		for count, pred, label in zip(counts, sorted_preds, labels):
-		    for idx in xrange(count):
-		        # print label[pred[idx]]
-		        tags = tags + label[pred[idx]] + ' '
-	        tags = tags.strip()
-		    submission_df.iloc[file_idx] = [file_name, tags]
+			for idx in xrange(count):
+				# print label[pred[idx]]
+				tags = tags + label[pred[idx]] + ' '
+		tags = tags.strip()
+		file_idx = re.findall('(\d+)', file_name)[0]
+		submission_df.loc[file_idx] = [file_name, tags]
+
+	print("Done with batch {}".format(batch))
 
 try:
-	pd.to_csv(os.path.join(root_dir, 'submission.csv'), submission_df)
+	submission_df.to_csv(submission_df_file, index=False)
 except Exception as e:
-	print("Can't save df due to some stupid fucking error. GO RATHER DIE.")
+	print("Error:", e)
 
 print("Done.")
--- src/models/test_model.py
+++ src/models/test_model.py
@@ -5,7 +5,7 @@ import keras
 from keras import backend as K
 from keras.models import load_model
 # from keras.optimizers import Adam
-import os, sys, gc
+import os, sys, gc, math
 import lmdb
 import cPickle as pickle
 import numpy as np
@@ -15,18 +15,18 @@ sys.path.append('/home/intern/satellite/amazon-satellite-imagery/')
 from datum_pb2 import Datum
 
 # to make sure tf uses cpu, cause gpu busy
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+# os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 # supress TF warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-model_savePath = '../../models/VGG19_model_4.hdf5'
-model_weights = '../../models/VGG19_12weights.08-1.88.hdf5'
-test_keys_pickle = '../../models/VGG19_5/test_keys.pkl'
-LMDB_DIR = '../../data/interim/lmdb/traindb'
+model_savePath = os.path.abspath('../../models/VGG19_model_4.hdf5')
+model_weights = os.path.abspath('../../models/VGG19_12/weights.44-2.53.hdf5')
+test_keys_pickle = os.path.abspath('../../models/VGG19_5/test_keys.pkl')
+LMDB_DIR = os.path.abspath('../../data/interim/lmdb/traindb')
 batch_size = 32
-root_dir = '../../models/VGG19_12/'
+root_dir = os.path.abspath('../../models/VGG19_12/')
 
 if os.path.exists(model_savePath):
 	model = load_model(model_savePath)
@@ -53,6 +53,10 @@ opt = keras.optimizers.SGD(lr=0.01, momentum=0.5, nesterov=True)
 model.compile(loss=loss_mapping, optimizer=opt, metrics=['accuracy'])
 print("Metrics for the model: ", model.metrics_names)
 
+atm_indices = np.array([1,4,7,9])
+common_indices = np.array([3,6,8,11,12,14,15])
+rare_indices = np.array([0,2,5,10,13,16])
+
 try:
 	env = lmdb.open(LMDB_DIR, max_dbs=3, readonly=True)
 except Exception as e:
@@ -69,14 +73,16 @@ inputCursor = txn.cursor(inputDBHandle)
 outputCursor = txn.cursor(outputDBHandle)
 
 print("Found {} files for testing".format(len(test_keys)))
-nb_batches = len(test_keys) // batch_size
+# an extra batch, if required, for the last samples
+nb_batches = int(math.ceil(len(test_keys)/(batch_size*1.0)))
 print("Testing {} batches".format(nb_batches))
 
 metrics = []
-metrics_pickle = root_dir + 'test_batch_metrics.pkl'
+metrics_pickle = os.path.join(root_dir, 'test_batch_metrics.pkl')
+FBetaScore_pickle = os.path.join(root_dir, 'fbeta_score.pkl')
 
-FBetaScores = []
-FBetaScores_pickle = root_dir + 'fbeta_batch_scores.pkl'
+y_true = []
+y_pred = []
 
 idx = 0
 for batch in xrange(1, nb_batches + 1):
@@ -86,7 +92,9 @@ for batch in xrange(1, nb_batches + 1):
 	batch_outputs_3 = []
 	outputs = []
 
-	for sample in xrange(batch_size):
+	nb_samples_in_batch = min(batch_size, len(test_keys) - idx)
+
+	for sample in xrange(nb_samples_in_batch):
 		key = test_keys[idx]
 		try:
 			raw_datum = inputCursor.get(str(key))
@@ -136,20 +144,20 @@ for batch in xrange(1, nb_batches + 1):
 		idx += 1
 
 	batch_inputs = np.array(batch_inputs)
-	batch_outputs_1 = np.asarray(batch_outputs_1, dtype='int')
-	oneHot_count_1 = np.zeros((batch_size, 5), dtype='int')
+	batch_outputs_1 = np.asarray(batch_outputs_1, dtype='uint8')
+	oneHot_count_1 = np.zeros((batch_outputs_1.shape[0], 5), dtype='int')
 	count_1 = batch_outputs_1.sum(axis=1)
-	oneHot_count_1[tuple(range(batch_size)), tuple(count_1)] = 1
+	oneHot_count_1[tuple(range(batch_outputs_1.shape[0])), tuple(count_1)] = 1
 
-	batch_outputs_2 = np.asarray(batch_outputs_2, dtype='int')
-	oneHot_count_2 = np.zeros((batch_size, 8), dtype='int')
+	batch_outputs_2 = np.asarray(batch_outputs_2, dtype='uint8')
+	oneHot_count_2 = np.zeros((batch_outputs_2.shape[0], 8), dtype='int')
 	count_2 = batch_outputs_2.sum(axis=1)
-	oneHot_count_2[tuple(range(batch_size)), tuple(count_2)] = 1
+	oneHot_count_2[tuple(range(batch_outputs_2.shape[0])), tuple(count_2)] = 1
 
-	batch_outputs_3 = np.asarray(batch_outputs_3, dtype='int')
-	oneHot_count_3 = np.zeros((batch_size, 7), dtype='int')
+	batch_outputs_3 = np.asarray(batch_outputs_3, dtype='uint8')
+	oneHot_count_3 = np.zeros((batch_outputs_3.shape[0], 7), dtype='int')
 	count_3 = batch_outputs_3.sum(axis=1)
-	oneHot_count_3[tuple(range(batch_size)), tuple(count_3)] = 1
+	oneHot_count_3[tuple(range(batch_outputs_3.shape[0])), tuple(count_3)] = 1
 
 	outputs.append(batch_outputs_1)
 	outputs.append(batch_outputs_2)
@@ -161,18 +169,18 @@ for batch in xrange(1, nb_batches + 1):
 	batch_preds = model.predict_on_batch(batch_inputs)
 	logs = model.test_on_batch(batch_inputs, outputs)
 
-	oneHot_preds = []
+	oneHot_preds_list = []
 	for preds in zip(*batch_preds):
 		sorted_preds = [np.argsort(pred)[::-1] for pred in preds[:3]]
-		oneHots = [np.zeros(pred.shape, dtype='int') for pred in sorted_preds]
+		oneHots = [np.zeros(pred.shape, dtype='uint8') for pred in sorted_preds]
 		counts = [np.argmax(np_count) for np_count in preds[3:]]
 		for count, pred, oneHot in zip(counts, sorted_preds, oneHots):
 		    for index in xrange(count):
 		        oneHot[pred[index]] = 1
 
-		oneHot_preds.append(oneHots)
+		oneHot_preds_list.append(oneHots)
 
-	oneHot_preds = np.array(oneHot_preds)
+	oneHot_preds = np.array(oneHot_preds_list)
 	shape = (oneHot_preds[:,0].shape[0], oneHot_preds[0,0].shape[0])
 	y_preds_1 = np.concatenate(oneHot_preds[:,0]).reshape(shape)
 
@@ -183,19 +191,57 @@ for batch in xrange(1, nb_batches + 1):
 	y_preds_3 = np.concatenate(oneHot_preds[:,2]).reshape(shape)
 
 	oneHot_preds = np.asarray(oneHot_preds)
-	fbeta_scores = []
-	fbeta_scores.append(fbeta_score(batch_outputs_1, y_preds_1, beta=2, average='samples'))
-	fbeta_scores.append(fbeta_score(batch_outputs_2, y_preds_2, beta=2, average='samples'))
-	fbeta_scores.append(fbeta_score(batch_outputs_3, y_preds_3, beta=2, average='samples'))
 
-	FBetaScores.append(fbeta_scores)
+	ground_truths = [batch_outputs_1, batch_outputs_2, batch_outputs_3]
+	predictions = [y_preds_1, y_preds_2, y_preds_3]
+
+	batch_y_pred = np.zeros((y_preds_1.shape[0], 17), dtype='uint8')
+	batch_y_true = np.zeros((y_preds_1.shape[0], 17), dtype='uint8')
+
+	indices = [np.array([1,3,4,9], dtype='int'), 
+	           np.array([3,6,8,11,12,14,15], dtype='int'), 
+	           np.array([0,2,5,10,13,16], dtype='int')]
+
+	for ground_truth, pred, index in zip(ground_truths, predictions, indices):
+	    batch_y_true[:, index] = ground_truth
+	    batch_y_pred[:, index] = pred
+
+	y_true.append(batch_y_true)
+	y_pred.append(batch_y_pred)
+
+	# fbeta_scores.append(fbeta_score(batch_outputs_1, y_preds_1, beta=2, average='samples'))
+	# fbeta_scores.append(fbeta_score(batch_outputs_2, y_preds_2, beta=2, average='samples'))
+	# fbeta_scores.append(fbeta_score(batch_outputs_3, y_preds_3, beta=2, average='samples'))
+
+	# FBetaScores.append(f2_score)
 	metrics.append(logs)
 	pickle.dump(metrics, open(metrics_pickle, 'w'))
-	pickle.dump(FBetaScores, open(FBetaScores_pickle, 'w'))
+	# pickle.dump(FBetaScores, open(FBetaScores_pickle, 'w'))
 	print("Metrics for batch {}:".format(batch), logs)
-	print("Fbeta scores for batch {}:".format(batch), fbeta_scores)
+	# print("Fbeta scores for batch {}:".format(batch), f2_score)
 
 	gc.collect()
+
+# make those lists into ndarrays
+shape = (len(test_keys), y_true[0].shape[-1])
+print("Shape for truths and preds:", shape)
+y_true_np = np.concatenate(y_true).reshape(shape)
+y_pred_np = np.concatenate(y_pred).reshape(shape)
+
+# get some free mem
+del y_true
+del y_pred
+gc.collect()
+
+# combined fbeta_score
+f2_score = fbeta_score(batch_y_true, batch_y_pred, beta=2, average='samples')
+print("F2 Score:", f2_score)
+try:
+	pickle.dump(f2_score, open(FBetaScore_pickle, 'wb'))
+	print("Dumped f2_score at:", FBetaScore_pickle)
+except Exception as e:
+	print("Can't dump the f2_score. Copy it from the terminal. :/")
+	print("Error:", e)
 
 print("Testing complete.")
 try:
